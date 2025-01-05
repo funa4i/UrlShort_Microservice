2321c175adf9c17d54a12605beeaa6b186861c6a
ne_kroman <ne_kroman@DESKTOP-B0TJ7PH>
2025-01-05 21:39:04 +0400

    Urlshort убрал лишние файлы

--- UrlShortService/src/main/java/org/urlshort/feign/clients/UserApplicationApi.java
+++ UrlShortService/src/main/java/org/urlshort/feign/clients/UserApplicationApi.java
@@ -4,7 +4,6 @@ package org.urlshort.feign.clients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 import org.urlshort.domain.data.DecreaseResultAnswer;
-import org.urlshort.feign.data.UserCreateRequest;
 
 @FeignClient(name = "user-server")
 public interface UserApplicationApi {
