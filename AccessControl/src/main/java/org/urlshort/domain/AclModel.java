73eb435cdd4dbfdc2f9c6e74100202c1b9ee90cd
ne_kroman <ne_kroman@DESKTOP-B0TJ7PH>
2025-01-29 17:35:41 +0400

    clean

--- AccessControl/src/main/java/org/urlshort/domain/AclModel.java
+++ AccessControl/src/main/java/org/urlshort/domain/AclModel.java
@@ -14,48 +14,50 @@ import org.urlshort.domain.repositories.UserRepository;
 import org.urlshort.exceptions.NullObjectException;
 import org.urlshort.exceptions.AlreadyExistsException;
 
-
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class AclModel {
 
     private final UserRepository userRep;
-
     private final PathRepository pathRep;
-
     private final RoleRepository roleRep;
 
     @Value("${role.default}")
     private String defaultRole;
 
     @Transactional
-    public void createUser(Long id){
-        if (userRep.existsById(id)){
+    public void createUser(Long id) {
+        if (userRep.existsById(id)) {
             throw new AlreadyExistsException(User.class, id.toString());
         }
-        var rl = roleRep.findByName(defaultRole).orElseThrow( () ->new RuntimeException("Incorrect default role"));
+        var rl = roleRep.findByName(defaultRole)
+                .orElseThrow(() -> new RuntimeException("Incorrect default role"));
         userRep.save(new User(id, rl));
     }
 
     @Transactional
-    public void setUserRole(Long id, String role){
-        var rl = roleRep.findByName(role).orElseThrow(() -> new NullObjectException(Role.class, role));
-        var user = userRep.findById(id).orElseThrow(() -> new NullObjectException(User.class, id.toString()));
+    public void setUserRole(Long id, String role) {
+        var rl = roleRep.findByName(role)
+                .orElseThrow(() -> new NullObjectException(Role.class, role));
+        var user = userRep.findById(id)
+                .orElseThrow(() -> new NullObjectException(User.class, id.toString()));
         user.setRole(rl);
     }
 
     @Transactional
-    public void deleteUser(Long id){
+    public void deleteUser(Long id) {
         userRep.deleteById(id);
     }
 
     @Transactional
-    public void addRoleForPath(String path, String role){
-        var newRole = roleRep.findByName(role).orElseThrow(() -> new NullObjectException(Role.class, role));
-        var newPath = pathRep.findByName(path).orElseThrow(() -> new NullObjectException(Path.class, path));
+    public void addRoleForPath(String path, String role) {
+        var newRole = roleRep.findByName(role)
+                .orElseThrow(() -> new NullObjectException(Role.class, role));
+        var newPath = pathRep.findByName(path)
+                .orElseThrow(() -> new NullObjectException(Path.class, path));
 
-        if (newRole.getPaths().contains(newPath)){
+        if (newRole.getPaths().contains(newPath)) {
             return;
         }
 
@@ -64,29 +66,24 @@ public class AclModel {
     }
 
     @Transactional
-    public void createPath(String path){
-        if (!pathRep.existsByName(path)){
+    public void createPath(String path) {
+        if (!pathRep.existsByName(path)) {
             pathRep.save(new Path(path));
         }
     }
 
     @Transactional
-    public boolean accessCheck(String checkPath, Long userId){
-        var user = userRep.findById(userId).orElseThrow(() ->
-                new NullObjectException(User.class, userId.toString())
-        );
-        return user
-                .getRole()
-                .getPaths()
-                .stream()
+    public boolean accessCheck(String checkPath, Long userId) {
+        var user = userRep.findById(userId)
+                .orElseThrow(() -> new NullObjectException(User.class, userId.toString()));
+        return user.getRole().getPaths().stream()
                 .map(Path::getName)
-                .anyMatch(x -> x.equals(checkPath)
-                );
+                .anyMatch(x -> x.equals(checkPath));
     }
 
     @Transactional
-    public void createRole(String role){
-        if (!roleRep.existsByName(role)){
+    public void createRole(String role) {
+        if (!roleRep.existsByName(role)) {
             roleRep.save(new Role(role));
             return;
         }
